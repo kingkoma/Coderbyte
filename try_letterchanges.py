b3a084c20513c37cd520b007d19cff2600c1a5fb
kingkoma <33335362+kingkoma@users.noreply.github.com>
2018-01-09 23:10:31 +0800

    Add files via upload

---
+++ try_letterchanges.py
@@ -0,0 +1,22 @@
+def LetterChanges(str):
+    new_str = ''
+    for i in str:
+        if i.isalpha():
+            if i == 'z':
+                i = 'a'
+            elif i == 'Z':
+                i = 'A'
+            else:
+                i = chr(ord(i) + 1)
+                if i in 'aeiou':
+                    i = i.upper()
+        else:
+            i = i
+        new_str += i
+    # code goes here
+    return new_str
+
+
+# keep this function call here
+print(LetterChanges(input()))
+
