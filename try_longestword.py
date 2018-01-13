97e74b6ee8c804d8775edf38b5289dfab5448640
kingkoma <33335362+kingkoma@users.noreply.github.com>
2018-01-13 13:46:05 +0800

    Update and rename try_longestword to try_longestword.py

--- try_longestword.py
+++ try_longestword.py
@@ -2,6 +2,7 @@
 Using the Python language, have the function LongestWord(sen) take the sen parameter being passed 
 and return the largest word in the string. If there are two or more words that are the same length, 
 return the first word from the string with that length. Ignore punctuation and assume sen will not be empty. 
+
 """
 
 def LongestWord(sen): 
