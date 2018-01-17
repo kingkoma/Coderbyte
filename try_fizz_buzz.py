fa49d4b0f405869ffe0e244b3f82ab98777a220c
kingkoma <33335362+kingkoma@users.noreply.github.com>
2018-01-17 16:53:29 +0800

    Create try_fizz_buzz.py

---
+++ try_fizz_buzz.py
@@ -0,0 +1,31 @@
+"""
+Given number n. Print number from 1 to n. But:
+when number is divided by 3, print "fizz".
+when number is divided by 5, print "buzz".
+when number is divided by both 3 and 5, print "fizz buzz".
+"""
+
+class Solution:
+    """
+    @param: n: An integer
+    @return: A list of strings.
+    """
+    def fizzBuzz(self, n):
+        # write your code here
+        li = []
+        for i in range(1, n+1):
+            if i%3 == 0 and i%5 != 0:
+                li.append('fizz')
+            elif i%5 == 0 and i%3 != 0:
+                li.append('buzz')
+            elif i%3 == 0 and i%5 == 0:
+                li.append('fizz buzz')
+            else:
+                li.append(str(i))
+        return li
+                        
+                        
+if __name__ == '__main__':
+    s = Solution()
+    s.fizzBuzz(15)
+    
